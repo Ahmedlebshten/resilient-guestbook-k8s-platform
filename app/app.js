b8de4dc4edb1f153ffb766346144ddd5d9340413
Ahmedlebshten <ahmedlebshtenlebshten@gmail.com>
2026-04-26 22:27:02 +0300

    edit src code

--- app/app.js
+++ app/app.js
@@ -32,7 +32,7 @@ app.get('/', async (req, res) => {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cloud Native Guestbook</title>
+        <title>Cloud Native GuestBook</title>
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body class="bg-gray-900 text-white min-h-screen font-sans">
